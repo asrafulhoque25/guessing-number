1dfc127de53b7f7ae654992b5b46b870a6cb0260
Asraful Hoque <96014879+asrafulhoque25@users.noreply.github.com>
2022-08-03 19:39:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [started, setStarted] = useState(false);
+  const [status, setStatus] = useState(false);
+  const [answer, setAnswer] = useState(0);
+  const [rightAnswer, setRightAnsweer] = useState(0);
 
+
+
+  const submit = (e) => {
+    e.preventDefault();
+    const formValid = +answer >= 0;
+    if (!formValid) {
+      return;
+    }
+    if (+answer === +rightAnswer) {
+      setStatus("you got it... Play again");
+      setStarted(false);
+    } else if (+answer < +rightAnswer) {
+      setStatus("too low");
+    } else {
+      setStatus("too high");
+    }
+  };
+
+  const start = () => {
+    setRightAnsweer(Math.ceil(Math.random() * 10));
+    setStarted(true);
+  };
+  if (started) {
+    return (
+      <div className='gameStart'>
+        <form onSubmit={submit}>
+          <div>
+            <h1 className='fs-2'>Insert Your Number Within 1 To 9</h1>
+            <input className='inputNumber' value={answer} onChange={(e) => setAnswer(e.target.value)} />
+          </div>
+          <button type="submit" className='mt-4 px-4 pb-2 fs-3 border-0 bg-info '>check</button>
+        </form>
+        <p className='fs-3 text-warning text-uppercase '>{status}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className='startBtn d-flex align-items-center justify-content-center flex-column'>
+        <button type="button" onClick={start} className="px-4 pb-2 fs-2 border-0 bg-warning  text-white">
+          start
+        </button>
+        <p className='fs-3'>{status}</p>
+      </div>
+    );
+  }
+
+
+  }
 export default App;
